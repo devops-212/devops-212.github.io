038343a665425fe85a4d32ddaf144b3ceba4176e
Emblack2103 <133470118+Emblack2103@users.noreply.github.com>
2025-06-19 22:22:39 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-if("serviceWorker"in navigator){
-    Window.addEvenlist("load",function(){
-        navigator.serviceWorker
-        .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered",err));
-        
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("Service Worker registrado"))
+      .catch(err => console.log("Service Worker NO registrado", err));
+  });
 }
